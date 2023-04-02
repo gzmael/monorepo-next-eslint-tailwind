f3b6d7ecb02390cddcea7385b5289a2e1fa855c2
Jezmael Basilio <jezmaelbasilio@gmail.com>
2023-04-02 20:23:59 -0300

    start repository

---
+++ apps/web/postcss.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const configs = require('tailwind-config/postcss.config')
+
+module.exports = {
+  ...configs,
+}
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { theme } = require('tailwind-config/tailwind.config')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    ...theme,
+  },
+  plugins: [],
+}
